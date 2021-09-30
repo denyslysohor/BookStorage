800a4cf68b45a011ed5b7deaff5167f48900014b
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-09-30 11:03:15 +0300

    General files of BookStorage

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStorage
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} {Description} {Author} {Price} {Year}";
+        }
+    }
+}
